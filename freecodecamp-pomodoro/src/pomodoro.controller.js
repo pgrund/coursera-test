28dce2e34727c4c29d32a248aba0ea5e1dff6de3
Peter Grund <peter.grund@bisnode.de>
2016-10-13 11:02:57 +0200

    finished pomodoro

--- freecodecamp-pomodoro/src/pomodoro.controller.js
+++ freecodecamp-pomodoro/src/pomodoro.controller.js
@@ -3,14 +3,15 @@
   angular.module("Pomodoro")
     .controller('PomodoroController', PomodoroController);
 
-  PomodoroController.$inject = ['$interval','$timeout'];
-  function PomodoroController($interval, $timeout) {
+  PomodoroController.$inject = ['$interval','$timeout', 'PomodoroTimerStep'];
+  function PomodoroController($interval, $timeout,PomodoroTimerStep) {
     var pomCtrl = this;
 
-    pomCtrl.sessionLength = .2; // 25;
-    pomCtrl.breakLength = .1; //5;
+    pomCtrl.sessionLength =  25;
+    pomCtrl.breakLength = 5;
     pomCtrl.running = false;
     pomCtrl.mode = "Session";
+    pomCtrl.perc = 0;
 
     pomCtrl.current = pomCtrl.sessionLength * 60;
 
@@ -18,52 +19,49 @@
       timeout : undefined,
       interval: undefined
     };
-    pomCtrl.setSession = function(val) {
+
+    var set = function(mode, val) {
       if(!pomCtrl.running) {
-        pomCtrl.mode = "Session";
+        pomCtrl.mode = mode;
         if(val){
-          var temp = pomCtrl.sessionLength + val;//Math.floor()
+          var temp = pomCtrl[mode.toLowerCase()+'Length'] + val;//Math.floor()
           if(temp <= 0) {
             console.log("invalid setting", temp);
           } else {
-            pomCtrl.sessionLength = temp;
-          }
+            pomCtrl[mode.toLowerCase()+'Length']  = temp;
+            }
         }
-        pomCtrl.current = pomCtrl.sessionLength * 60;
+        pomCtrl.perc = 0;
+        pomCtrl.current = pomCtrl[mode.toLowerCase()+'Length']  * 60;
       } else {
-        console.log("clock ist still running, cannot set session");
+        console.log("clock ist still running, cannot set " + mode);
       }
     }
-    pomCtrl.setBreak = function(val) {
-      if(!pomCtrl.running) {
-        pomCtrl.mode = "Break";
-        if(val) {
-          var temp = pomCtrl.sessionLength + val;//Math.floor()
-          if(temp <= 0) {
-            console.log("invalid setting", temp);
-          } else {
-            pomCtrl.breakLength = temp;
-          }
-        }
-        pomCtrl.current = pomCtrl.breakLength * 60;
-      } else {
-        console.log("clock ist still running, cannot set session");
-      }
+    pomCtrl.sessionIncr = function() {
+      set("Session", PomodoroTimerStep);
+    }
+    pomCtrl.sessionDecr = function() {
+      set("Session", -1*PomodoroTimerStep);
+    }
+    pomCtrl.breakIncr = function() {
+      set("Break", PomodoroTimerStep);
+    }
+    pomCtrl.breakDecr = function() {
+      set("Break", -1*PomodoroTimerStep);
     }
     pomCtrl.toggleTimer = function() {
       pomCtrl.running = !pomCtrl.running;
       if(pomCtrl.running) {
         //start
+        var max = (pomCtrl.mode == "Session" ? pomCtrl.sessionLength : pomCtrl.breakLength) * 60;
         promises.interval = $interval( function() {
           pomCtrl.current--;
-          pomCtrl.perc = Math.floor(((pomCtrl.mode == "Session" ? pomCtrl.sessionLength : pomCtrl.breakLength) * 60 - pomCtrl.current) / (pomCtrl.mode == "Session" ? pomCtrl.sessionLength * 60: pomCtrl.breakLength * 60));
+          pomCtrl.perc = Math.floor((max - pomCtrl.current) / max * 100);
         }, 1000);
         promises.timeout = $timeout(function() {
           pomCtrl.toggleTimer();
+          set(pomCtrl.mode == "Break" ? "Session": "Break");
           if(pomCtrl.mode == "Break"){
-            pomCtrl.setSession();
-          } else {
-            pomCtrl.setBreak();
             pomCtrl.toggleTimer();
           }
         }, pomCtrl.current * 1000);
