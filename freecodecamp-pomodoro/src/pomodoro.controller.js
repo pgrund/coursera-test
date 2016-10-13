b5add0b6088b7638ac3c76e65a1e2ef64cd86d02
Peter Grund <peter.grund@bisnode.de>
2016-10-13 08:23:10 +0200

    added +-

--- freecodecamp-pomodoro/src/pomodoro.controller.js
+++ freecodecamp-pomodoro/src/pomodoro.controller.js
@@ -7,9 +7,10 @@
   function PomodoroController($interval, $timeout) {
     var pomCtrl = this;
 
-    pomCtrl.sessionLength = 25;
-    pomCtrl.breakLength = 5;
+    pomCtrl.sessionLength = .2; // 25;
+    pomCtrl.breakLength = .1; //5;
     pomCtrl.running = false;
+    pomCtrl.mode = "Session";
 
     pomCtrl.current = pomCtrl.sessionLength * 60;
 
@@ -17,13 +18,55 @@
       timeout : undefined,
       interval: undefined
     };
+    pomCtrl.setSession = function(val) {
+      if(!pomCtrl.running) {
+        pomCtrl.mode = "Session";
+        if(val){
+          var temp = pomCtrl.sessionLength + val;//Math.floor()
+          if(temp <= 0) {
+            console.log("invalid setting", temp);
+          } else {
+            pomCtrl.sessionLength = temp;
+          }
+        }
+        pomCtrl.current = pomCtrl.sessionLength * 60;
+      } else {
+        console.log("clock ist still running, cannot set session");
+      }
+    }
+    pomCtrl.setBreak = function(val) {
+      if(!pomCtrl.running) {
+        pomCtrl.mode = "Break";
+        if(val) {
+          var temp = pomCtrl.sessionLength + val;//Math.floor()
+          if(temp <= 0) {
+            console.log("invalid setting", temp);
+          } else {
+            pomCtrl.breakLength = temp;
+          }
+        }
+        pomCtrl.current = pomCtrl.breakLength * 60;
+      } else {
+        console.log("clock ist still running, cannot set session");
+      }
+    }
     pomCtrl.toggleTimer = function() {
       pomCtrl.running = !pomCtrl.running;
       if(pomCtrl.running) {
         //start
-        pomCtrl.current = pomCtrl.sessionLength * 60;
-        promises.interval = $interval( function() { pomCtrl.current--; }, 1000);
-        promises.timeout = $timeout(pomCtrl.toggleTimer, pomCtrl.current * 1000);
+        promises.interval = $interval( function() {
+          pomCtrl.current--;
+          pomCtrl.perc = Math.floor(((pomCtrl.mode == "Session" ? pomCtrl.sessionLength : pomCtrl.breakLength) * 60 - pomCtrl.current) / (pomCtrl.mode == "Session" ? pomCtrl.sessionLength * 60: pomCtrl.breakLength * 60));
+        }, 1000);
+        promises.timeout = $timeout(function() {
+          pomCtrl.toggleTimer();
+          if(pomCtrl.mode == "Break"){
+            pomCtrl.setSession();
+          } else {
+            pomCtrl.setBreak();
+            pomCtrl.toggleTimer();
+          }
+        }, pomCtrl.current * 1000);
       } else {
         // stop
         $interval.cancel(promises.interval);
